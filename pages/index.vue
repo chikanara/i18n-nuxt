ddcae38487adccff8e1caee9353129221d698b97
wassimTekab <wassim.ahmed@tekab.dev>
2022-01-24 23:08:11 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1 class="title">{{ $t('message') }}</h1>
+    ********************************
+    <!-- some code -->
+    <SelectComponent />
+  </div>
+</template>
+
+<script lang="ts">
+import SelectComponent from '../components/LanguageInput.vue'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    SelectComponent,
+  },
+}
+</script>
